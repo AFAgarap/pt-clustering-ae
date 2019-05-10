f756011f2814bc2bd6093c336d40d0b8810551bc
Abien Fred Agarap <abien_agarap@dlsu.edu.ph>
2019-05-11 00:43:14 +0800

    docs : add license header

--- models/kmeans.py
+++ models/kmeans.py
@@ -1,3 +1,19 @@
+# Using latent code from variational autoencoder for clustering
+# Copyright (C) 2019  Abien Fred Agarap
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Implementation of k-Means Clustering in scikit-learn"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
