2027a870b9d24413bc9eb29c4b5d982497efba05
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-03-24 14:24:42 +0800

    docs: add docstring for epoch_train()

--- clustering_ae/modules/autoencoder.py
+++ clustering_ae/modules/autoencoder.py
@@ -79,6 +79,21 @@ class Autoencoder(nn.Module):
 
 
 def epoch_train(model, data_loader):
+    """
+    Trains a model for one epoch.
+
+    Parameters
+    ----------
+    model : torch.nn.Module
+        The model to train.
+    data_loader : torch.utils.dataloader.DataLoader
+        The data loader object that consists of the data pipeline.
+
+    Returns
+    -------
+    epoch_loss : float
+        The epoch loss.
+    """
     epoch_loss = 0
     for batch_features, batch_labels in data_loader:
         model.optimizer.zero_grad()
