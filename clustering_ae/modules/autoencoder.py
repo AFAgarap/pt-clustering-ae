14b7cf30ca7595dbaf46d3184fd2e2633d38f1a2
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-03-24 14:26:38 +0800

    refactor: reformat autoencoder module

--- clustering_ae/modules/autoencoder.py
+++ clustering_ae/modules/autoencoder.py
@@ -21,27 +21,33 @@ import torch.nn as nn
 class Autoencoder(nn.Module):
     def __init__(self, **kwargs):
         super().__init__()
-        self.encoder_layers = torch.nn.ModuleList([
-            torch.nn.Linear(in_features=kwargs["input_shape"], out_features=500),
-            torch.nn.ReLU(),
-            torch.nn.Linear(in_features=500, out_features=500),
-            torch.nn.ReLU(),
-            torch.nn.Linear(in_features=500, out_features=2000),
-            torch.nn.ReLU(),
-            torch.nn.Linear(in_features=2000, out_features=kwargs["code_dim"]),
-            torch.nn.Sigmoid()
-        ])
-        self.decoder_layers = torch.nn.ModuleList([
-            torch.nn.Linear(in_features=kwargs["code_dim"], out_features=2000),
-            torch.nn.ReLU(),
-            torch.nn.Linear(in_features=2000, out_features=500),
-            torch.nn.ReLU(),
-            torch.nn.Linear(in_features=500, out_features=500),
-            torch.nn.ReLU(),
-            torch.nn.Linear(in_features=500, out_features=kwargs["input_shape"]),
-            torch.nn.Sigmoid()
-        ])
-        self.optimizer = torch.optim.Adam(params=self.parameters(), lr=kwargs["learning_rate"])
+        self.encoder_layers = torch.nn.ModuleList(
+            [
+                torch.nn.Linear(in_features=kwargs["input_shape"], out_features=500),
+                torch.nn.ReLU(),
+                torch.nn.Linear(in_features=500, out_features=500),
+                torch.nn.ReLU(),
+                torch.nn.Linear(in_features=500, out_features=2000),
+                torch.nn.ReLU(),
+                torch.nn.Linear(in_features=2000, out_features=kwargs["code_dim"]),
+                torch.nn.Sigmoid(),
+            ]
+        )
+        self.decoder_layers = torch.nn.ModuleList(
+            [
+                torch.nn.Linear(in_features=kwargs["code_dim"], out_features=2000),
+                torch.nn.ReLU(),
+                torch.nn.Linear(in_features=2000, out_features=500),
+                torch.nn.ReLU(),
+                torch.nn.Linear(in_features=500, out_features=500),
+                torch.nn.ReLU(),
+                torch.nn.Linear(in_features=500, out_features=kwargs["input_shape"]),
+                torch.nn.Sigmoid(),
+            ]
+        )
+        self.optimizer = torch.optim.Adam(
+            params=self.parameters(), lr=kwargs["learning_rate"]
+        )
         self.criterion = torch.nn.BCELoss()
 
     def forward(self, features):
@@ -106,4 +112,3 @@ def epoch_train(model, data_loader):
         epoch_loss += train_loss.item()
     epoch_loss /= len(data_loader)
     return epoch_loss
-
